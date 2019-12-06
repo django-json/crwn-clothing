dbdab4ee3880dd8a9f6009f9ff78ded7251c956f
django-json <diango.jayson@gmail.com>
2019-12-06 13:39:17 +0800

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
+import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => {
+	return(
+		<div>
+			<h1>Hats Page</h1>
+		</div>
+	)
+};
 
 class App extends Component {
 	render() {     
 		return (
 			<div>
-				<HomePage />
+				<Switch>
+					<Route exact path='/' component={ HomePage }/>
+					<Route path='/shop/hats' component={ HatsPage }/>
+				</Switch>
 			</div>
 		);
 	}
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,8 +49,13 @@ class Directory extends Component {
 		return (
 			<div className='directory-menu'>
 				{
-					this.state.sections.map(({ title, imageUrl, size, id, linkUrl }) => {
-						return(<MenuItem key={ id } title={ title } imageUrl={ imageUrl } size={ size }/>)
+					this.state.sections.map(({ id, ...otherSectionProps }) => {
+						return(
+							<MenuItem 
+							key={ id } 
+							{ ...otherSectionProps }
+							/>
+						)
 					})
 				}
 			</div>
