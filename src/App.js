524b7050a9ceb69512526b9c3842afe8ff75833a
django-json <diango.jayson@gmail.com>
2019-12-16 14:45:38 +0800

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 class App extends Component {
 	render() {     
 		return (
 			<div>
+				<Header />
 				<Switch>
 					<Route exact path='/' component={ HomePage }/>
 					<Route path='/shop' component={ ShopPage }/>
