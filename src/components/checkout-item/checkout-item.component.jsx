2b2b40c52eff625140b780dafeca56c5c383576f
django-json <diango.jayson@gmail.com>
2019-12-28 16:43:52 +0800

    added addItem and removeItem functionality to checkout item component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import { removeItem } from '../../redux/cart/cart.actions';
+import { addItem } from '../../redux/cart/cart.actions';
+
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, quantity, price, imageUrl }}) => (
-	<div className='checkout-item'>
-		<div className='image-container'>
-			<img src={imageUrl} alt='item'/>
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+	const { name, quantity, price, imageUrl } = cartItem;
+	return (
+		<div className='checkout-item'>
+			<div className='image-container'>
+				<img src={imageUrl} alt='item'/>
+			</div>
+			<div className='name'>{name}</div>
+			<div className='quantity'>
+				<div 
+					onClick={() => removeItem(cartItem)}
+					className='arrow'
+				>
+					&#10094;
+				</div>
+				<span className='value'>{quantity}</span>
+				<div 
+					onClick={() => addItem(cartItem)}
+					className='arrow'
+				>
+					&#10095;
+				</div>
+			</div>
+			<div className='price'>${price}</div>
+			<div 
+				className='remove-button'
+				onClick={() => clearItem(cartItem)}
+			>
+				&#10005;
+			</div>
 		</div>
-		<div className='name'>{name}</div>
-		<div className='quantity'>{quantity}</div>
-		<div className='price'>${price}</div>
-		<div className='remove-button'>&#10005;</div>
-	</div>
-);
-export default CheckoutItem;
+	)
+};
+
+const mapDispatchToProps = dispatch => ({
+	clearItem: item => dispatch(clearItemFromCart(item)),
+	addItem: item => dispatch(addItem(item)),
+	removeItem: item => dispatch(removeItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,15 +1,48 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
+	/*
+		If a new cart item to be added exists in the cart items already, that cart item will be stored in the existingCartItem variable which will be used for future condition.
+	*/
 	const existingCartItem = cartItems.find(
 		cartItem => cartItem.id === cartItemToAdd.id
 	);
 
+	/*
+		If existingCartItem is not undefined, .map() modifies the quantity by adding 1 to that specific cartItem's quantity that meets the condition.
+	*/
 	if(existingCartItem) {
 		return cartItems.map(cartItem => 
-			cartItem.id === cartItemToAdd.id
-				? { ...cartItem, quantity: cartItem.quantity + 1 }
-				: cartItem
+			cartItem.id === cartItemToAdd.id ? 
+				{ ...cartItem, quantity: cartItem.quantity + 1 }
+			: 	cartItem
 		);
 	}
 
+	/*
+		Else if existingCartItem is undefined, return the existing cart items with the new cart item object which has a new property quantity of 1 as a base quantity value.
+	*/
 	return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+	const existingCartItem = cartItems.find(
+		cartItem => cartItem.id === cartItemToRemove.id
+	);
+
+	/*
+		If existing cart item has a quantity of 1, then it will be removed from the checkout items
+	*/
+	if(existingCartItem.quantity === 1) {
+		return cartItems.filter(
+			cartItem => cartItem.id !== cartItemToRemove.id
+		);
+	}
+
+	/*
+		Else, the quantity of that cart item will decrease by 1 instead.
+	*/
+	return cartItems.map(cartItem => 
+		cartItem.id === cartItemToRemove.id ?
+			{ ...cartItem, quantity: cartItem.quantity - 1}
+		: 	cartItem
+	)
 };
