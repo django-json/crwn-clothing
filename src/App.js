28f89ef4c102614acdf391f61b694b1b2c9af2fb
django-json <diango.jayson@gmail.com>
2019-12-28 14:09:36 +0800

    added checkout page, checkout item and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 import { setCurrentUser } from './redux/user/user.actions';
 
@@ -48,6 +53,7 @@ class App extends Component {
 				<Switch>
 					<Route exact path='/' component={HomePage}/>
 					<Route path='/shop' component={ShopPage}/>
+					<Route exact path='/checkout' component={CheckoutPage}/>
 					<Route 
 						exact 
 						path='/signin' 
@@ -63,8 +69,8 @@ class App extends Component {
 	}
 }
 
-const mapStateToProps = state => ({
-	currentUser: state.user.currentUser
+const mapStateToProps = createStructuredSelector ({
+	currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
